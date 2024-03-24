d01ac78aa57fbf9f9c370d5cb2c003c752368d77
arandolan <aran_dolan@yahoo.co.uk>
2024-03-24 14:24:38 +0100

    Remove link to old page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -32,9 +32,7 @@ export default async function ListTransactions() {
             <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
               <div className="text-center space-y-2 sm:text-left">
                 <div className="space-y-0.5">
-                  <p className="text-lg text-black font-semibold">
-                    <Link href={route + t.id}>{t.title}</Link>
-                  </p>
+                  <p className="text-lg text-black font-semibold">{t.title}</p>
                   <p className="text-slate-500 font-medium">{t.description}</p>
                 </div>
                 <DeleteButton
